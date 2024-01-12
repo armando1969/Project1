dbf6373038a7cde3654c9049ac828fe9d7473c42
Armando Gallegos <Florencio.Gallegos@mobileconsultingsolutions.us>
2024-01-12 12:00:40 -0800

    adding the contentview.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentages = 20
+    
+    let tipPerccentages = [10, 15, 20, 25, 0]
+    var totalPerperson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPaid = grandTotal / Double(numberOfPeople)
+        
+        return(amountPaid)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) People")
+                        }
+                    }
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip Percentage", selection: $tipPercentages) {
+                        ForEach(8..<tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    } .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section {
+                    Text(totalPerperson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }.navigationTitle("We Split").navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
